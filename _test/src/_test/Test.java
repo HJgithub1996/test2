f87afe6283d5328ee5e3fb08369d907c783125cf
TEDU <TEDU@DESKTOP-UO8OA1M>
2020-01-03 19:52:57 +0800

    1

--- _test/src/_test/Test.java
+++ _test/src/_test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		String s = "hhhhh";
 		String a = "abc";
+		System.out.println(s);
+		System.out.println(a+s);
 		System.out.println(a);
 	}
     
